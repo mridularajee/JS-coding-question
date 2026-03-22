376259c6f144ce2be7b83d5364d6a685f92ac2c7
Mridula Singh <mridularaje@gmail.com>
2026-03-22 19:50:15 +0530

    'loop'

---
+++ loop/level_1/ques.js
@@ -0,0 +1,30 @@
+//print 10 to 56
+// for(let i=10; i<=56; i++){
+//     console.log(i)
+// }
+
+//print 22 to 78
+// for(let i=10; i<=56; i++){
+//     console.log(i)
+// }
+
+//print 54 to 122
+// for(let i=10; i<=56; i++){
+//     console.log(i)
+// }
+
+//print 32 to 98
+// for(let i=10; i<=56; i++){
+//     console.log(i)
+// }
+
+//print 10 to 2
+// for(let i=10; i>=2; i--){
+//     console.log(i)
+// } 
+
+let obj = {a:1, b:2};
+
+for (let key in obj) {
+  console.log(key, obj[key]);
+}
